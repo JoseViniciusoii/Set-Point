4bf1dec6374c1a29efadbd0ea0076cdaac9d6610
JoseViniciusoii <seu-email-do-github@email.com>
2026-05-14 14:26:26 -0300

    Commit pfv

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,9 +3,9 @@
  * Este arquivo estabelece a conexão entre o seu Frontend (Vite/React) 
  * e o Banco de Dados (PostgreSQL no Supabase).
  */
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-all'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+   const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+   const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+   export const supabase = createClient(supabaseUrl, supabaseAnonKey);
